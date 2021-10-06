e15d6864aa7c55e4f9155f3beb87605907ebfec3
catstacks <61471777+catstacks@users.noreply.github.com>
2021-10-06 10:12:59 +0100

    added debugging exercises

---
+++ qa_debugging.py
@@ -0,0 +1,33 @@
+import pdb
+
+# pdb.run('<a statement>')
+
+# pdb.set_trace()
+
+# a = "aaa"
+# b = "bbb"
+# c = "ccc"
+
+# final = a + b + c
+# print(final)
+
+# a = "aaa"
+# b = "bbb"
+# c = "ccc"
+
+# def final(var1,var2,var3):
+#     pdb.set_trace()
+#     return(var1+var2+var3)
+
+# print(final(a,b,c))
+
+# user_funds = 10.31
+# item_price = price["Burger"]
+
+# if item_price < user_funds:
+#     Print("You have enough money!")
+# if item_price = user_funds:
+#     print("You have the precise amount of money")
+# if item_price < user_funds:
+#     print(Sorry you don't have enough money)
+
