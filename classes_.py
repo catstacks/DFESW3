559a4b3af15cf3da0f5933445dc83d8ade91bfbd
catstacks <61471777+catstacks@users.noreply.github.com>
2021-10-07 09:24:00 +0100

    Added classes exercises

---
+++ classes_.py
@@ -0,0 +1,37 @@
+# Tutorial code below
+
+# class Student:
+
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+# John = Student("John", "21")
+# Jane = Student("Jane", "22")
+
+# print(getattr(John, "age"))
+
+# Exercise attempt
+
+# class students:
+
+#     def __init__(self, name, age, class_ = 'student'):
+#         self.name = name
+#         self.age = age
+#         self.class_= class_
+
+#     def test_score(score1, score2, score3):
+#         return (score1 + score2 + score3)/3
+
+# student1 = students()
+
+class Letterchecker():
+    vowels = 'aeiou'
+    def checkthing(self,x):
+        return x.lower() in self.vowels
+vowelchecker = Letterchecker()
+for i in 'dog':
+    print(vowelchecker.checkthing(i))
+
+
+
