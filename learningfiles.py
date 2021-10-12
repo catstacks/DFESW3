2c8b766ced556180ebbb2da8b515d7624dad7c06
catstacks <61471777+catstacks@users.noreply.github.com>
2021-10-12 11:37:45 +0100

    learning how to manipulate files in python

--- learningfiles.py
+++ learningfiles.py
@@ -1,13 +1,15 @@
-file = open("filename.txt", "r")
+file_name = open("filename.txt", "r")
 
-outfile = ""
+# outfile = ""
 
-for line in range(1, 10):
-    if line % 2 == 0:
-        outfile += file.readline()
-    else:
-        file.readline()
+# for line in range(1, 10):
+#     if line % 2 == 0:
+#         outfile += file_name.readline()
+#     else:
+#         file_name.readline()
 
-file = open("filename.txt", "w")
-file.write(outfile)
-file.close()
+#file_name = open("filename.txt", "w")
+# file_name.write(outfile) 
+# file_name.close()
+
+print(file_name.read()) #this works when line one is uncommented
